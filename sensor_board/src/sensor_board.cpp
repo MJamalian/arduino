14f86d3d1d4b2701b7dd45e5556777fc3393002b
mohammad <mohjam77@gmail.com>
2019-02-23 00:03:34 +0330

    Sensors connected to the sensor board.

--- sensor_board/src/sensor_board.cpp
+++ sensor_board/src/sensor_board.cpp
@@ -1,13 +1,24 @@
 #include <arduino.h>
+#include <LM35.h>
+#include <Ultrasonic.h>
 
+#define TRIG_PIN 12
+#define ECHO_PIN 13
 
-void setup(){
+LM35 temp(A0);
+Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN);
 
+void setup()
+{
+    Serial.begin(9600);
 }
 
+void loop()
+{
+    // Serial.print(temp.cel());
+    // Serial.print(" ");
+    Serial.print(ultrasonic.Ranging(CM));
+    Serial.print(" ");
 
-void loop(){
-
-
-
+    delay(1000);
 }
