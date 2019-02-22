1d1efb1b8dc83b1ea2f355783ca48b3363b6bea0
mohammad <mohjam77@gmail.com>
2019-02-22 21:59:02 +0330

    LCD connected

--- main_board/src/main_board.cpp
+++ main_board/src/main_board.cpp
@@ -1,9 +1,11 @@
 #include <arduino.h>
-
+#include <LiquidCrystal.h>
 int incomingByte;
-
+LiquidCrystal lcd(12, 11, 10, 7, 6, 5, 4);
 void setup(){
     Serial.begin(9600);
+    lcd.begin(20, 4);
+    lcd.print("hello world!");
 }
 
 
